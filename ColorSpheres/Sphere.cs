f382822d269d7419b97572231aa95149955653b5
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-04-03 10:34:09 +0100

    Create private values and constructor Sphere

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -2,6 +2,15 @@ namespace ColorSpheres
 {
     public class Sphere
     {
-        
+        private int radius;
+        private Color color;
+
+        public Sphere(int radius, Color color)
+        {
+            this.radius = radius;
+            this.color = color;
+
+            
+        }
     }
 }
