1f2d31c4d5680637d7fa64d32cc0c63d7d5a6a78
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-04-03 10:41:45 +0100

    Create methods Pop, Throw and GetTimesThrown

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -4,13 +4,31 @@ namespace ColorSpheres
     {
         private int radius;
         private Color color;
+        private int throwCount;
 
         public Sphere(int radius, Color color)
         {
             this.radius = radius;
             this.color = color;
+            this.throwCount = 0;
+        }
 
-            
+        public void Pop()
+        {
+            this.radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (this.radius > 0)
+            {
+                this.throwCount++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return this.throwCount;
         }
     }
 }
